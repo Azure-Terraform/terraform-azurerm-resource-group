453440a7959afb1cb5f1ea4889be8b7742fe3d24
Hiscocks, Joshua <joshua.hiscocks@lexisnexisrisk.com>
2020-05-22 10:21:01 +0100

    Resource group type added to name

--- variables.tf
+++ variables.tf
@@ -11,9 +11,10 @@ variable "tags" {
 variable "names" {
   description = "Names to be applied to resources (inclusive)"
   type        = object({
-                  environment  = string
-                  location     = string
-                  market       = string
-                  product_name = string
+                  environment         = string
+                  location            = string
+                  market              = string
+                  product_name        = string
+                  resource_group_type = string
                 })
 }
