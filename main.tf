141cfb975b8b45251eafc681ae68daf08788b6d3
Hiscocks, Joshua <joshua.hiscocks@lexisnexisrisk.com>
2020-05-22 10:08:58 +0100

    Region added to resource group name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  resource_group_name = "${var.names.resource_group_type}-${var.names.product_name}-${var.names.environment}-${var.names.azure_region}"
+  resource_group_name = "${var.names.resource_group_type}-${var.names.product_name}-${var.names.environment}-${var.names.location}"
 }
 
 resource "azurerm_resource_group" "rg" {
