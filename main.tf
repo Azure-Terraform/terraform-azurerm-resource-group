40f787f90d281e67a626ec566fa82a5786da070e
wilsonnm02 <77080362+wilsonnm02@users.noreply.github.com>
2021-01-06 16:44:58 -0600

    Update main.tf
    The azurerm 2.0.0 provider now requires features to be defined, even if it's an empty list. Without it this example doesn't work with 2.0.0+

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ locals {
   resource_group_name = "${var.names.resource_group_type}-${var.names.product_name}-${var.names.environment}-${var.names.location}"
 }
 
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = local.resource_group_name
   location = var.location
