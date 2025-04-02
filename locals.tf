88d7e879704afd0b70181c3c1fc54d3485b63dde
Venkata Dorisala <dorisalv@risk.regn.net>
2025-04-02 13:56:53 +0100

    Fixed invalid declaration.

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   resource_type_safe  = lower(join("-", var.resource_type == "" ? [] : [var.resource_type]))
-  resource_group_name = "${var.names.resource_group_type}-${var.names.product_name}-${var.names.environment}-${var.names.location}${resource_type_safe}"
+  resource_group_name = "${var.names.resource_group_type}-${var.names.product_name}-${var.names.environment}-${var.names.location}${local.resource_type_safe}"
   unique_name         = var.unique_name == "true" ? random_integer.suffix[0].result : (var.unique_name == "false" ? null : var.unique_name)
 }
